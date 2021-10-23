fab384c48592cdedad7d8c27788c425a13c397ee
asatokoki <okometabero@me.com>
2021-10-23 14:32:59 +0000

    共通部分できた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/create'
   get 'homes/top'
   devise_for :users
   root to: 'homes#top'
+  resources :books
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
