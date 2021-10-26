f60db60f84858afb023a2a8b8799f5af8500b6f2
asatokoki <okometabero@me.com>
2021-10-26 05:46:52 +0000

    ログイんサイんインの後の画面移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
   get '/homes/about', to: 'homes#about', as: :about
+  devise_for :users
 
   resources :books
   resources :users
