0a2ad895eaa0458a2080e9ba4e7ab016217fb89f
asatokoki <okometabero@me.com>
2021-10-26 12:13:33 +0000

    本とユーザーのバリでしょん完璧や。阻止したった。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,21 @@ class UsersController < ApplicationController
 
   	@user = User.find(params[:id])
   	 if current_user.id == @user.id
-  	   
+  	   render "users/edit"
+
   	 else
-  	   redirect_to user_path(current_user.id)
+  	 redirect_to user_path(current_user.id)
+
   	 end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to  user_path(@user.id)
-
+    if @user.update(user_params)
+      redirect_to  user_path(current_user)
+    else
+      render 'users/edit'
+    end
   end
 
   private
